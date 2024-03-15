6e7101e02128e86169fe5f0877a0a5d605148a70
guanli1105 <guanli1105@gmail.com>
2024-03-15 09:28:53 +0800

    修改vite配置 实现分类打包

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,23 @@ import path from "path"
 export default defineConfig({
     root: path.join( __dirname, 'src' ),
     build: {
-        outDir: '../',
+        outDir: __dirname,
         rollupOptions: {
             input: path.join( __dirname, 'src/index.html' ),
             output: {
-                entryFileNames: 'assets/[name].js',
+                // 入口文件名
+                entryFileNames: 'assets/js/[name].js',
+                // 块文件名
+                chunkFileNames: chunkInfo => {
+                    const [ name, ext ] = path.basename( chunkInfo.name ).split( '.' );
+                    return `assets/js/${ name.toLocaleLowerCase() }.js`;
+                },
+                // 资源文件名 css 图片等等
+                assetFileNames: chunkInfo => {
+                    const [ name, ext ] = path.basename( chunkInfo.name ).split( '.' );
+                    let fold = ext === 'css' ? 'css' : 'images';
+                    return `assets/${ fold }/${ name.toLocaleLowerCase() }.${ ext }`;
+                }
             }
         }
     },

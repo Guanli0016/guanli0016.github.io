dcaf4c0609411e71471a5374a589c3db9a4e10e0
guanli1105 <guanli1105@gmail.com>
2024-04-15 16:21:24 +0800

    初始化docs目录

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+    // 应用级配置选项
+    lang: 'en-US',
+    title: 'Guanli Site',
+    titleTemplate: 'Guanli HomePage',
+    description: 'liuguanli homepage',
+})

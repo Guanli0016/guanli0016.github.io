77f68fe694a6661ea0c95521eb1e4950f5d4e08e
guanli1105 <guanli1105@gmail.com>
2024-04-15 17:25:32 +0800

    创建docs首页

--- docs-src/.vitepress/config.ts
+++ docs-src/.vitepress/config.ts
@@ -4,7 +4,35 @@ export default defineConfig({
     // 应用级配置选项
     lang: 'en-US',
     title: 'Guanli Site',
-    titleTemplate: 'Guanli HomePage',
+    titleTemplate: ':title | Guanli HomePage',
     description: 'liuguanli homepage',
-    outDir: '/docs'
+    outDir: '../docs',
+    appearance: 'dark',
+
+    // 主题相关配置
+    themeConfig: {
+        i18nRouting: false,
+        logo: '/images/liu-logo.jpg',
+        siteTitle: '刘冠立的个人主页',
+        nav: [
+            {
+                text: '收藏',
+                items: [
+                    { text: 'vite教程', link: '/guides/vite' },
+                ]
+            }, {
+                text: '家庭',
+                items: [
+                    { text: '照片', link: '/guides/vite' },
+                ]
+            }
+        ],
+        socialLinks: [
+            { icon: 'github', link: 'https://github.com/Guanli0016/' },
+            { icon: 'npm', link: 'https://www.npmjs.com/~guanli0016' },
+        ],
+        footer: {
+            copyright: 'Copyright © 2024-present Guanli'
+        },
+    }
 })
--- docs-src/index.md
+++ docs-src/index.md
@@ -1,1 +1,18 @@
-# 我是标题
+---
+layout: home
+
+hero:
+  name: 刘冠立
+  text: 我的个人主页
+  tagline: 事能知足心常泰，人到无求品自高
+  image:
+    src: /images/logo.png
+    alt: Guanli
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /guide/what-is-vitepress
+    - theme: alt
+      text: Github地址
+      link: https://github.com/Guanli0016/
+---
